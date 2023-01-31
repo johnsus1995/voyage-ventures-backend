a5e04036fabaab93820d73c889c6a65016fe7f6f
Jaison John <jaisonjohny26@gmail.com>
2023-01-31 08:14:18 +0530

    signin functionality added

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import { signUp } from "../controllers/user.js";
+import { signin, signUp } from "../controllers/user.js";
 
 router.post("/signup", signUp);
+router.post("/signin", signin);
 
 export default router;
