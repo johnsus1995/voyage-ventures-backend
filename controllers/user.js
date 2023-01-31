c31465ec75b28d14f332357a7eef054176bd665f
Jaison John <jaison.john@whiterabbit.group>
2023-01-31 16:34:54 +0530

    test commit ssh

--- controllers/user.js
+++ controllers/user.js
@@ -26,6 +26,7 @@ export const signUp = async (req, res) => {
     res.status(201).json({ newUser, token });
   } catch (error) {
     res.status(500).json({ message: "something went wrong with signup" });
+    //
     console.log(error);
   }
 };
