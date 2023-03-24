16b41a433e069adfc560e5f8cbc3a6e228a7e8db
Jaison John <jaisonjohny26@gmail.com>
2023-03-24 09:44:05 +0530

    clean up

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
-import UserModel from "../models/User.js";
 
 const auth = async (req, res, next) => {
   try {
-    console.log(req.headers["authorization"])
     let headers = req.headers["authorization"]
     let token = headers.split(" ")[1];
     // return
@@ -13,11 +11,6 @@ const auth = async (req, res, next) => {
     if (token && isCustomAuth) {
       decodedData = jwt.verify(token, process.env.SECRET_KEY);
       req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-      const googleId = decodedData?.sub.toString();
-      const user = await UserModel.findOne({ googleId });
-      req.userId = user?._id;
     }
     next();
   } catch (error) {
